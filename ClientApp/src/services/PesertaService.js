18b61ecd0ddadba35fbb7fd17326a469e4a3ee1a
mdestafadilah <desta.08b@gmail.com>
2023-02-01 14:58:56 +0700

    MOCKUP CONSUMING API

---
+++ ClientApp/src/services/PesertaService.js
@@ -0,0 +1,11 @@
+export async function getAllPeserta() {
+    // TODO
+}
+
+export async function createPeserta(data) {
+    // TODO
+}
+
+export async function deletePeserta(id) {
+    // TODO
+}
---
+++ ClientApp/src/services/TestService.js
@@ -0,0 +1,11 @@
+export async function getAllTest() {
+    // TODO
+}
+
+export async function createTest(data) {
+    // TODO
+}
+
+export async function deleteTest(id) {
+    // TODO
+}

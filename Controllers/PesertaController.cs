dd809e8e370934f6fb6af64b1f1afe42a52a46b1
mdestafadilah <desta.08b@gmail.com>
2023-02-06 13:09:24 +0700

    CRUD PESERTA DONE

--- Controllers/PesertaController.cs
+++ Controllers/PesertaController.cs
@@ -23,42 +23,42 @@ public class PesertaController : ControllerBase
         return Ok(pesertas);
     }
 
-    // [HttpGet]
-    // [Route("api/[controller]/id")]
-    // public async Task<IActionResult> GetPesertaByidAsync(int id) 
-    // {
-    //     var peserta = await _myCrudDbContext.Peserta.FindAsync(id);
-    //     return Ok(peserta);
-    // }
+    [HttpGet]
+    [Route("id")]
+    public async Task<IActionResult> GetPesertaByidAsync(string id) 
+    {
+        var peserta = await _myCrudDbContext.Peserta.FindAsync(id);
+        return Ok(peserta);
+    }
 
-    // [HttpPost]
-    // public async Task<IActionResult> PostPesertaAsync(Peserta peserta)
-    // {
-    //     _myCrudDbContext.Peserta.Add(peserta);
-    //     await _myCrudDbContext.SaveChangesAsync();
-    //     return Created($"api/[controller]/id?id={peserta.Id}", peserta);    
-    // }
+    [HttpPost]
+    public async Task<IActionResult> PostPesertaAsync(Peserta peserta)
+    {
+        _myCrudDbContext.Peserta.Add(peserta);
+        await _myCrudDbContext.SaveChangesAsync();
+        return Created($"id?id={peserta.Id}", peserta);    
+    }
 
-    // [HttpPut]
-    // public async Task<IActionResult> PutPesertaAsync(Peserta pesertaToUpdate)
-    // {
-    //     _myCrudDbContext.Peserta.Update(pesertaToUpdate);
-    //     await _myCrudDbContext.SaveChangesAsync();
-    //     return NoContent();
-    // }
+    [HttpPut]
+    public async Task<IActionResult> PutPesertaAsync(Peserta pesertaToUpdate)
+    {
+        _myCrudDbContext.Peserta.Update(pesertaToUpdate);
+        await _myCrudDbContext.SaveChangesAsync();
+        return NoContent();
+    }
 
-    // [Route("api/[controller]/id")]
-    // [HttpDelete]
-    // public async Task<IActionResult> DeletePesertaAsync(int id)
-    // {
-    //     var pesertaToDelete = await _myCrudDbContext.Peserta.FindAsync(id);
-    //     if (pesertaToDelete == null)
-    //     {
-    //         return NotFound();
-    //     }
+    [Route("id")]
+    [HttpDelete]
+    public async Task<IActionResult> DeletePesertaAsync(string id)
+    {
+        var pesertaToDelete = await _myCrudDbContext.Peserta.FindAsync(id);
+        if (pesertaToDelete == null)
+        {
+            return NotFound();
+        }
 
-    //     _myCrudDbContext.Peserta.Remove(pesertaToDelete);
-    //     await _myCrudDbContext.SaveChangesAsync();
-    //     return NoContent();
-    // }
+        _myCrudDbContext.Peserta.Remove(pesertaToDelete);
+        await _myCrudDbContext.SaveChangesAsync();
+        return NoContent();
+    }
 };
